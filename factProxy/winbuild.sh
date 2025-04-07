321843386090bbae6cc09a92ff74a42b261adb15
Distortions81 <carlotto81@gmail.com>
2025-04-07 16:30:14 -0600

    add connect links

--- factProxy/winbuild.sh
+++ factProxy/winbuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 GOOS=windows GOARCH=amd64 go build -o M45-Factorio-Proxy.exe
-zip winProxy.zip M45-Factorio-Proxy.exe readme.txt
+zip winProxy.zip M45-Factorio-Proxy.exe readme.txt connect-links.html
