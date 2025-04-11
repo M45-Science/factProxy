19c669f12f278f3b320433a9d30ae88fd16e2ba3
Distortions81 <carlotto81@gmail.com>
2025-04-11 15:10:03 -0600

    async read all ports

--- relayClient/local.go
+++ relayClient/local.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"net"
 )
 
 func handleGameListens() {
 	for _, port := range listeners {
-		buf := make([]byte, 2048)
-		for {
-			n, addr, err := port.ReadFromUDP(buf)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Read error on %v: %v\n", port.LocalAddr(), err)
-				return
+		go func(p *net.UDPConn) {
+			buf := make([]byte, 65536)
+			for {
+				n, addr, err := p.ReadFromUDP(buf)
+				if err != nil {
+					//fmt.Fprintf(os.Stderr, "Read error on %v: %v\n", p.LocalAddr(), err)
+					return
+				}
+				fmt.Printf("Received %d bytes from %v on %v: %s\n", n, addr, p.LocalAddr(), string(buf[:n]))
 			}
-			fmt.Printf("Received %d bytes from %v on %v: %s\n", n, addr, port.LocalAddr(), string(buf[:n]))
-		}
+		}(port)
 	}
 }
